610b3495316139bc596d88d9838cb0245eaba457
bednarra <Bednar@lanit.ru>
2024-11-28 20:11:31 +0300

    new step

--- conftest.py
+++ conftest.py
@@ -3,8 +3,18 @@ from selenium import webdriver
 from lib.gostteam.fixtures import gost_team_page
 
 
+# @pytest.fixture(scope='module')
+# def browser():
+#     driver = webdriver.Chrome()
+#     yield driver
+#     driver.quit()
+
 @pytest.fixture(scope='module')
 def browser():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Remote(
+        command_executor='http://chrome:4444/wd/hub',
+        options=options
+    )
     yield driver
     driver.quit()
