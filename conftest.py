8485cde34736d54afe306e3b389cac5e6cd3b36a
bednarra <Bednar@lanit.ru>
2024-11-28 20:29:38 +0300

    Updated

--- conftest.py
+++ conftest.py
@@ -5,7 +5,13 @@ from lib.gostteam.fixtures import gost_team_page
 
 @pytest.fixture(scope='module')
 def browser():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
