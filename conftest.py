672509188e869c01f1cbe2476516769fa9c5598e
bednarra <Bednar@lanit.ru>
2024-11-28 20:26:41 +0300

    Updated

--- conftest.py
+++ conftest.py
@@ -3,20 +3,9 @@ from selenium import webdriver
 from lib.gostteam.fixtures import gost_team_page
 
 
-# @pytest.fixture(scope='module')
-# def browser():
-#     driver = webdriver.Chrome()
-#     yield driver
-#     driver.quit()
-
 @pytest.fixture(scope='module')
 def browser():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Remote(
-        command_executor='http://chrome:4444/wd/hub',
-        options=options
-    )
+    driver = webdriver.Chrome()
     yield driver
     driver.quit()
+
