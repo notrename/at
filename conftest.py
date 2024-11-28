530de06129b08de7fc12c6a705f1c0a8fb0ea65b
bednarra <Bednar@lanit.ru>
2024-11-28 20:20:28 +0300

    new step

--- conftest.py
+++ conftest.py
@@ -12,6 +12,8 @@ from lib.gostteam.fixtures import gost_team_page
 @pytest.fixture(scope='module')
 def browser():
     options = webdriver.ChromeOptions()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Remote(
         command_executor='http://chrome:4444/wd/hub',
         options=options
